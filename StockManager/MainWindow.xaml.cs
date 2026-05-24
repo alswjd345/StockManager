67819d1fba1e70ec1f5acf3f322c98fd3f52ba91
mjk7306@baver.com <mjk7306@naver.com>
2026-05-24 23:19:42 +0900

    feat: 비품관리 기본 UI 및 조회 기능 구현

---
+++ StockManager/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using StockManager.Models;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using StockManager.Models;
+
+namespace StockManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            add_data();
+            Get_Category();
+        }
+        private void add_data()
+        {
+            List<AssetItem> list = new List<AssetItem>();
+            AssetItem item = new AssetItem();
+            item.Name = "마우스";
+            item.Category = "사무용품";
+
+            list.Add(item);
+            gridresult.ItemsSource = list;
+
+        }
+        private void Get_Category()
+        {
+            
+            ComboCate.Items.Add("사무용품");
+            ComboCate.Items.Add("가구");
+            ComboCate.Items.Add("IT용품");
+            ComboCate.Items.Add("기타");
+            ComboCate.SelectedIndex = 0;
+
+            ComboCate2.Items.Add("사무용품");
+            ComboCate2.Items.Add("가구");
+            ComboCate2.Items.Add("IT용품");
+            ComboCate2.Items.Add("기타");
+            ComboCate2.SelectedIndex = 0;
+        }
+    }
+ 
+}
---
+++ StockManager/Models/AssetItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockManager.Models
+{
+    public class AssetItem
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+        public string Location { get; set; }
+        public string Status { get; set; }
+        public string Memo { get; set; }
+    }
+}
