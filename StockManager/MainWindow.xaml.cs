b9ab0a7d3e630c616057de85307865e89ef95715
mjk7306@baver.com <mjk7306@naver.com>
2026-05-26 20:51:22 +0900

    26.05.26 삭제 기능 추가

--- StockManager/MainWindow.xaml.cs
+++ StockManager/MainWindow.xaml.cs
@@ -67,13 +67,18 @@ namespace StockManager
             DataTable dt = new DataTable();
             dt.Load(reader);
 
+            dt.Columns.Add("CHK", typeof(bool));
+            foreach (DataRow row in dt.Rows)
+            {
+                row["CHK"] = false;
+            }
             gridresult.ItemsSource = dt.DefaultView;
-
+            
             sql.Close();
 
 
 
-            sql.Close();
+       
 
         }
         private void Get_Category()
@@ -129,6 +134,43 @@ namespace StockManager
 
                 sql.Close();
             }
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("정말 삭제하시겠습니까?","삭제 확인", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                DataView view = gridresult.ItemsSource as DataView;
+                foreach (DataRowView row in view)
+                {
+                    bool isChecked = (bool)row["CHK"];
+                    if (isChecked == true)
+                    {
+                        SqliteConnection sql = new SqliteConnection("Data Source=Equipment.db");
+                        sql.Open();
+
+                        int id = Convert.ToInt32(row["ID"]);
+                        string SQL = $@"
+                        DELETE FROM Equipment
+                        WHERE ID = {id}";
+                        
+
+                        SqliteCommand cmd = new SqliteCommand(SQL, sql);
+                        cmd.ExecuteNonQuery();
+
+                        
+
+                        sql.Close();
+                       
+                    }
+                }
+
+                //CheckBox.Ischecked
+            }
+            MessageBox.Show("삭제 완료");
+            Get_List();
         }
     }
  
